f62cbcb2ff1af0e059490bd64dc3cffe0b991271
PHUNG XUAN VUONG(Phung Xuan Vuong <phungvi@lgcns.com>
2025-11-03 08:56:36 +0700

    remove unused

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,18 +1,10 @@
 // src/App.tsx
 
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import cloudflareLogo from "./assets/Cloudflare_Logo.svg";
-import honoLogo from "./assets/hono.svg";
 import "./App.css";
-import Layout from "./components/Layout";
 import Index from "./pages";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("unknown");
 
   return (
     <AuthProvider>
